5389cd59b6ffdb908adffd20f3cbefb727a78696
alekoChe <aleko2117bis@gmail.com>
2021-12-10 18:40:25 +0300

    семафор в тоннеле

--- src/main/java/ru/gbModule3/Tunnel.java
+++ src/main/java/ru/gbModule3/Tunnel.java
@@ -6,7 +6,7 @@ import static ru.gbModule3.RaceApp.CARS_COUNT;
 
 public class Tunnel extends Stage{
 
-    //Semaphore limitNumberCarInTunnel = new Semaphore(CARS_COUNT / 2);
+    Semaphore limitNumberCarInTunnel = new Semaphore(CARS_COUNT / 2);
     public Tunnel() {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
@@ -16,14 +16,14 @@ public class Tunnel extends Stage{
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
-                //limitNumberCarInTunnel.acquire();
+                limitNumberCarInTunnel.acquire();
                 System.out.println(c.getName() + " начал этап: " + description);
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
                 System.out.println(c.getName() + " закончил этап: " + description);
-                //limitNumberCarInTunnel.release();
+                limitNumberCarInTunnel.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
